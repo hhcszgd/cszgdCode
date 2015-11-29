3e9566ea8843e1f7ee05a7a5a2bbd60fa4a317e3
960770164 <960770164@qq.com>
2015-11-29 14:44:02 +0800

    创建欢迎界面控制器

--- weibo1/weibo1/classes/Module/NewFeatureCollectionVC.swift
+++ weibo1/weibo1/classes/Module/NewFeatureCollectionVC.swift
@@ -7,9 +7,9 @@
 //
 
 import UIKit
-
+import SnapKit
 private let reuseIdentifier = "Cell"
-
+let imemCount = 4
 class NewFeatureCollectionVC: UICollectionViewController {
 
 
@@ -22,6 +22,8 @@ class NewFeatureCollectionVC: UICollectionViewController {
         layout1.scrollDirection = UICollectionViewScrollDirection.Horizontal
     super.init(collectionViewLayout: layout1)
         collectionView?.pagingEnabled = true//要放在父类初始化方法以后, 才会有collectionView
+        collectionView?.bounces = false
+        collectionView?.showsHorizontalScrollIndicator = false
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -58,7 +60,7 @@ class NewFeatureCollectionVC: UICollectionViewController {
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 5
+        return imemCount
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -66,9 +68,20 @@ class NewFeatureCollectionVC: UICollectionViewController {
     
         // Configure the cell
         cell.backgroundColor = indexPath.item % 2 == 0 ? UIColor.redColor() : UIColor.greenColor()
+        cell.idx = indexPath.item
+        
+        
         return cell
     }
-
+    override func collectionView(collectionView: UICollectionView, didEndDisplayingCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.item)
+        let cell = collectionView.visibleCells().last as! CustomCell
+        let index = collectionView.indexPathForCell(cell)
+        let indexItem = index?.item ?? 0
+        if indexItem == imemCount - 1 {//判断是不是最后一页
+            cell.btnStartAnimat()
+        }
+    }
     // MARK: UICollectionViewDelegate
 
     /*
@@ -104,16 +117,56 @@ class NewFeatureCollectionVC: UICollectionViewController {
 
 //MARK: 自定义cell
 class CustomCell : UICollectionViewCell {
+    //添加一个记录下标的属性用来标记图片
+    var idx = 0{
+        didSet{
+             imgView.image = UIImage(named: "new_feature_\(idx + 1)")/////////////
+            btn.hidden = true
+//            if idx == 3 {btn.hidden = false}
+        }
+    }
+    
     //添加一个imageView属性和按钮属性
+    lazy var imgView = UIImageView(image: UIImage(named: "new_feature_1"))
+    lazy var btn : UIButton = {
+        var temp = UIButton()
+        temp.setTitle("点击体验", forState: UIControlState.Normal)
+        temp.setBackgroundImage(UIImage(named: "new_feature_finish_button"), forState: .Normal)
+
+        temp.setBackgroundImage(UIImage(named: "new_feature_finish_button_highlighted"), forState: .Highlighted)
+        temp.sizeToFit()
+        return temp
+    }()
+    //调整每个cell的图片
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(imgView)
+        layoutUI()
     }
-
+    func layoutUI(){
+        contentView.addSubview(imgView)
+        contentView.addSubview(btn)
+        imgView.snp_makeConstraints { (make) -> Void in
+            make.edges.equalTo(contentView.snp_edges)
+        }
+        btn.snp_makeConstraints { (make) -> Void in
+            make.centerX.equalTo(contentView.snp_centerX)
+            make.centerY.equalTo(contentView.snp_bottom).offset(-110)
+        }
+    
+    }
+    func btnStartAnimat (){
+        btn.hidden = false
+        btn.transform = CGAffineTransformMakeScale(0, 0)
+        UIView.animateWithDuration(2.0, delay: 0, options: [], animations: { () -> Void in
+            //执行动画
+            self.btn.transform = CGAffineTransformMakeScale(1, 1)
+            }) { (_ ) -> Void in
+                //啥也不淡
+        }
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    lazy var imgView = UIImageView(image: UIImage(named: "new_feature_1"))
     
     
 
